257f741bbee7cfb292a301ce76fa3bbc23e0ed9a
Rustam Shorov <rustam.shorov.by@gmail.by>
2024-03-23 10:51:23 +0300

    skeleton_added

---
+++ currency_test/currency_test/currency_testApp.swift
@@ -0,0 +1,32 @@
+//
+//  currency_testApp.swift
+//  currency_test
+//
+//  Created by Rustam Shorov on 23.03.24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct currency_testApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
